0ffdc620abe42f5e3e08fb6c83a3b3fd47b19f19
Marvin-21 <66485174+Marvin-21@users.noreply.github.com>
2020-08-25 18:53:34 -0600

    nuevas vistas
    Se crearon las Vistas de Tipos de Producto

--- src/vista/JIntFrmInsertarTipoProducto.java
+++ src/vista/JIntFrmInsertarTipoProducto.java
@@ -9,12 +9,12 @@ package vista;
  *
  * @author Marvin Joel
  */
-public class JIntFrmModificarPuesto extends javax.swing.JInternalFrame {
+public class JIntFrmInsertarTipoProducto extends javax.swing.JInternalFrame {
 
     /**
-     * Creates new form JIntFrmModificarPuesto
+     * Creates new form JIntFrmInsertarTipoProducto
      */
-    public JIntFrmModificarPuesto() {
+    public JIntFrmInsertarTipoProducto() {
         initComponents();
     }
 
@@ -30,82 +30,82 @@ public class JIntFrmModificarPuesto extends javax.swing.JInternalFrame {
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLblCodigo = new javax.swing.JLabel();
+        jTxtTipoProductoID = new javax.swing.JTextField();
         jLblCodigo1 = new javax.swing.JLabel();
-        jLblCodigo2 = new javax.swing.JLabel();
-        jTxtPuestoID = new javax.swing.JTextField();
         jTxtNombre = new javax.swing.JTextField();
-        jTxtDireccion = new javax.swing.JTextField();
+        jLblCodigo2 = new javax.swing.JLabel();
+        jTxtDescripcion = new javax.swing.JTextField();
+        jBtnInsertarTipoProducto = new javax.swing.JButton();
         jBtnRegresar = new javax.swing.JButton();
-        jBtnModificarPuesto = new javax.swing.JButton();
 
         jPanel1.setBackground(new java.awt.Color(102, 255, 255));
 
         jLabel1.setFont(new java.awt.Font("Times New Roman", 3, 24)); // NOI18N
-        jLabel1.setText("Modificar Puesto");
+        jLabel1.setText("Insertar Tipo Producto");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(118, 118, 118)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(142, 142, 142))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabel1)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         jLblCodigo.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-        jLblCodigo.setText("Puesto ID:");
+        jLblCodigo.setText("Tipo Producto ID:");
+
+        jTxtTipoProductoID.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
 
         jLblCodigo1.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         jLblCodigo1.setText("Nombre:");
 
-        jLblCodigo2.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-        jLblCodigo2.setText("Direccion:");
-
-        jTxtPuestoID.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-
         jTxtNombre.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
 
-        jTxtDireccion.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jLblCodigo2.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jLblCodigo2.setText("Descripcion:");
+
+        jTxtDescripcion.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+
+        jBtnInsertarTipoProducto.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jBtnInsertarTipoProducto.setText("Insertar");
 
         jBtnRegresar.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         jBtnRegresar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/boton_volver.jpg"))); // NOI18N
         jBtnRegresar.setText("Regresar");
 
-        jBtnModificarPuesto.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-        jBtnModificarPuesto.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/modificar.jpg"))); // NOI18N
-        jBtnModificarPuesto.setText("Modificar");
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jBtnModificarPuesto, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(jBtnRegresar))
-                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLblCodigo)
                             .addComponent(jLblCodigo1)
                             .addComponent(jLblCodigo2))
-                        .addGap(69, 69, 69)
+                        .addGap(76, 76, 76)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jTxtPuestoID, javax.swing.GroupLayout.DEFAULT_SIZE, 207, Short.MAX_VALUE)
+                            .addComponent(jTxtTipoProductoID, javax.swing.GroupLayout.DEFAULT_SIZE, 207, Short.MAX_VALUE)
                             .addComponent(jTxtNombre)
-                            .addComponent(jTxtDireccion))))
-                .addContainerGap(57, Short.MAX_VALUE))
+                            .addComponent(jTxtDescripcion)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(104, 104, 104)
+                        .addComponent(jBtnInsertarTipoProducto, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(49, 49, 49)
+                        .addComponent(jBtnRegresar)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -114,7 +114,7 @@ public class JIntFrmModificarPuesto extends javax.swing.JInternalFrame {
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLblCodigo)
-                    .addComponent(jTxtPuestoID, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jTxtTipoProductoID, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLblCodigo1)
@@ -122,12 +122,12 @@ public class JIntFrmModificarPuesto extends javax.swing.JInternalFrame {
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLblCodigo2)
-                    .addComponent(jTxtDireccion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jBtnModificarPuesto, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTxtDescripcion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(37, 37, 37)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jBtnInsertarTipoProducto, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jBtnRegresar))
-                .addGap(0, 38, Short.MAX_VALUE))
+                .addGap(0, 18, Short.MAX_VALUE))
         );
 
         pack();
@@ -135,15 +135,15 @@ public class JIntFrmModificarPuesto extends javax.swing.JInternalFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    public javax.swing.JButton jBtnModificarPuesto;
+    public javax.swing.JButton jBtnInsertarTipoProducto;
     public javax.swing.JButton jBtnRegresar;
     private javax.swing.JLabel jLabel1;
     public javax.swing.JLabel jLblCodigo;
     public javax.swing.JLabel jLblCodigo1;
     public javax.swing.JLabel jLblCodigo2;
     private javax.swing.JPanel jPanel1;
-    public javax.swing.JTextField jTxtDireccion;
+    public javax.swing.JTextField jTxtDescripcion;
     public javax.swing.JTextField jTxtNombre;
-    public javax.swing.JTextField jTxtPuestoID;
+    public javax.swing.JTextField jTxtTipoProductoID;
     // End of variables declaration//GEN-END:variables
 }
--- src/vista/JIntFrmModificarTipoProducto.java
+++ src/vista/JIntFrmModificarTipoProducto.java
@@ -9,12 +9,12 @@ package vista;
  *
  * @author Marvin Joel
  */
-public class JIntFrmModificarPuesto extends javax.swing.JInternalFrame {
+public class JIntFrmModificarTipoProducto extends javax.swing.JInternalFrame {
 
     /**
-     * Creates new form JIntFrmModificarPuesto
+     * Creates new form JIntFrmModificarTipoProducto
      */
-    public JIntFrmModificarPuesto() {
+    public JIntFrmModificarTipoProducto() {
         initComponents();
     }
 
@@ -32,80 +32,80 @@ public class JIntFrmModificarPuesto extends javax.swing.JInternalFrame {
         jLblCodigo = new javax.swing.JLabel();
         jLblCodigo1 = new javax.swing.JLabel();
         jLblCodigo2 = new javax.swing.JLabel();
-        jTxtPuestoID = new javax.swing.JTextField();
+        jTxtTipoProductoID = new javax.swing.JTextField();
         jTxtNombre = new javax.swing.JTextField();
-        jTxtDireccion = new javax.swing.JTextField();
+        jTxtDescripcion = new javax.swing.JTextField();
+        jBtnModificarTipoProducto = new javax.swing.JButton();
         jBtnRegresar = new javax.swing.JButton();
-        jBtnModificarPuesto = new javax.swing.JButton();
 
         jPanel1.setBackground(new java.awt.Color(102, 255, 255));
 
         jLabel1.setFont(new java.awt.Font("Times New Roman", 3, 24)); // NOI18N
-        jLabel1.setText("Modificar Puesto");
+        jLabel1.setText("Modificar Tipo Producto");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(118, 118, 118)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(100, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(142, 142, 142))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabel1)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         jLblCodigo.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-        jLblCodigo.setText("Puesto ID:");
+        jLblCodigo.setText("Tipo Producto ID:");
 
         jLblCodigo1.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         jLblCodigo1.setText("Nombre:");
 
         jLblCodigo2.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-        jLblCodigo2.setText("Direccion:");
+        jLblCodigo2.setText("Descripcion:");
 
-        jTxtPuestoID.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jTxtTipoProductoID.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
 
         jTxtNombre.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
 
-        jTxtDireccion.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jTxtDescripcion.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+
+        jBtnModificarTipoProducto.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jBtnModificarTipoProducto.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/modificar.jpg"))); // NOI18N
+        jBtnModificarTipoProducto.setText("Modificar");
 
         jBtnRegresar.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         jBtnRegresar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/boton_volver.jpg"))); // NOI18N
         jBtnRegresar.setText("Regresar");
 
-        jBtnModificarPuesto.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-        jBtnModificarPuesto.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/modificar.jpg"))); // NOI18N
-        jBtnModificarPuesto.setText("Modificar");
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jBtnModificarPuesto, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(jBtnRegresar))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLblCodigo)
-                            .addComponent(jLblCodigo1)
-                            .addComponent(jLblCodigo2))
-                        .addGap(69, 69, 69)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jTxtPuestoID, javax.swing.GroupLayout.DEFAULT_SIZE, 207, Short.MAX_VALUE)
-                            .addComponent(jTxtNombre)
-                            .addComponent(jTxtDireccion))))
-                .addContainerGap(57, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLblCodigo)
+                    .addComponent(jLblCodigo1)
+                    .addComponent(jLblCodigo2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jTxtTipoProductoID, javax.swing.GroupLayout.DEFAULT_SIZE, 207, Short.MAX_VALUE)
+                    .addComponent(jTxtNombre)
+                    .addComponent(jTxtDescripcion))
+                .addGap(30, 30, 30))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(52, 52, 52)
+                .addComponent(jBtnModificarTipoProducto, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jBtnRegresar)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -114,7 +114,7 @@ public class JIntFrmModificarPuesto extends javax.swing.JInternalFrame {
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLblCodigo)
-                    .addComponent(jTxtPuestoID, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jTxtTipoProductoID, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLblCodigo1)
@@ -122,12 +122,12 @@ public class JIntFrmModificarPuesto extends javax.swing.JInternalFrame {
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLblCodigo2)
-                    .addComponent(jTxtDireccion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
+                    .addComponent(jTxtDescripcion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 31, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jBtnModificarPuesto, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jBtnModificarTipoProducto, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jBtnRegresar))
-                .addGap(0, 38, Short.MAX_VALUE))
+                .addGap(32, 32, 32))
         );
 
         pack();
@@ -135,15 +135,15 @@ public class JIntFrmModificarPuesto extends javax.swing.JInternalFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    public javax.swing.JButton jBtnModificarPuesto;
+    public javax.swing.JButton jBtnModificarTipoProducto;
     public javax.swing.JButton jBtnRegresar;
     private javax.swing.JLabel jLabel1;
     public javax.swing.JLabel jLblCodigo;
     public javax.swing.JLabel jLblCodigo1;
     public javax.swing.JLabel jLblCodigo2;
     private javax.swing.JPanel jPanel1;
-    public javax.swing.JTextField jTxtDireccion;
+    public javax.swing.JTextField jTxtDescripcion;
     public javax.swing.JTextField jTxtNombre;
-    public javax.swing.JTextField jTxtPuestoID;
+    public javax.swing.JTextField jTxtTipoProductoID;
     // End of variables declaration//GEN-END:variables
 }
